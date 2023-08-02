fb39decf4f1ee71fa345359bfc0578bca807cd27
septia <dewisantikaseptia@gmail.com>
2023-08-02 13:31:34 +0800

    initial commit

---
+++ src/MahasiswaBeraksi.java
@@ -0,0 +1,11 @@
+
+public class MahasiswaBeraksi {
+     public static void main(String[] args) {
+        Mahasiswa Septia = new Mahasiswa ("2110010022","Septia","TI");
+        System.out.println("Nama Mahasiswa = "+ Septia.getNama());
+        System.out.println("Npm Mahasiswa = "+ Septia.getNpm());
+        System.out.println("Prodi Mahasiswa = "+ Septia.getProdi());
+        
+        
+    }
+}
